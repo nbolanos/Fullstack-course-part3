e261bd74e5bb50d5a926708dd4bce73ae0ca6a9a
nikko bolaños <nikkobolanos@nikkos-MacBook-Pro.local>
2025-03-18 12:20:19 +0100

    added cors to index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
 let persons = [
     { 
@@ -39,6 +40,7 @@ morgan.token('body', (req, res) => {
   return JSON.stringify(req.body)
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
