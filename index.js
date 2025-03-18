575385f565a05ced3971d4dec90718969b9d4c47
nikko bolaños <nikkobolanos@nikkos-MacBook-Pro.local>
2025-03-18 16:34:19 +0100

    production code

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ morgan.token('body', (req, res) => {
   return JSON.stringify(req.body)
 })
 
+app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
